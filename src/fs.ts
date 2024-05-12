57f1dbce5a9c5532b26146ffc17be165b2d1ea87
carmine2001 <carminemn01@gmail.com>
2024-05-12 19:03:01 +0200

    stop-e-code-1

---
+++ src/fs.ts
@@ -0,0 +1,86 @@
+import fs from "node:fs";
+import path from "node:path";
+import { DateTime } from "luxon";
+import chalk from "chalk";
+
+const dataEntity = (ms:number) =>  {
+    
+    const dateTime  = DateTime.fromMillis(ms);
+    const dataAttuale = DateTime.now();
+    let differenzaMesi = Math.floor(dataAttuale.diff(dateTime).as("months"));
+    let differenzaGiorni = Math.floor(dataAttuale.diff(dateTime).as("days"));
+    let differenzaMinuti = Math.floor(dataAttuale.diff(dateTime).as("minutes"));
+    let differenzaOre = Math.floor(dataAttuale.diff(dateTime).as("hours"));
+    let differenzaSecondi = Math.floor(dataAttuale.diff(dateTime).as("seconds"));
+    let differenzaMs = Math.floor(dataAttuale.diff(dateTime).as("milliseconds"));
+    let tempo:number | string = differenzaSecondi;
+
+    if (differenzaSecondi >= 60){
+        tempo = `${differenzaMinuti} minuti fa`
+    }
+    if(differenzaMinuti >= 60){
+        tempo = `${differenzaOre} ore fa`
+    }
+    if(differenzaOre >= 24){
+        tempo = `${differenzaGiorni} giorni fa`
+    }
+    if(differenzaGiorni >= 31){
+        tempo = `${differenzaMesi} mesi fa`
+    }
+    if(differenzaMesi >= 24){
+        tempo = dateTime.toFormat("MM-dd-yyyy");
+    }
+
+    return tempo;
+}
+
+
+let percorso = process.cwd();
+let cartella = process.argv[2];
+
+console.log(chalk.green(`|--${cartella}`));
+
+let count = 2;
+
+    const openDir = (dir:any,count:number) => {
+    
+        if(dir.isDirectory()){
+            const readDir = fs.readdirSync(path.join(dir.path,dir.name),{withFileTypes:true});
+            readDir.forEach((dir) => {
+                if(dir.isDirectory()){
+                    const dirStat = fs.statSync(path.join(dir.path,dir.name));
+                    console.log(chalk.green("  |".repeat(count),dir.name));
+                    openDir(dir,count+1)
+                }
+                else{
+                    const fileStat = fs.statSync(path.join(dir.path,dir.name));
+                    const array = dataEntity(fileStat.mtimeMs).toLocaleString().split(" ");
+                    if(array.includes("ore") && +array[0] < 24){
+                        console.log(chalk.red("  |".repeat(count),dir.name,dataEntity(fileStat.mtimeMs)));
+                    }
+                    else if(array.includes("giorni") && +array[0] < 2){
+                        console.log(chalk.blue("  |".repeat(count),dir.name,dataEntity(fileStat.mtimeMs)));
+                    }
+                    else{
+                        console.log("  |".repeat(count),dir.name,dataEntity(fileStat.mtimeMs));
+                    }
+                            
+                        
+                }
+            })
+        }
+    } 
+
+    export default () => {
+        const readDir = fs.readdirSync(path.join(percorso,cartella),{withFileTypes:true});
+    
+        readDir.forEach(element => {
+            if(element.isDirectory()){
+                console.log(chalk.green(`${"  |".repeat(count)}${element.name}`));
+                openDir(element,count+1)
+            }else{
+                const fileStat = fs.statSync(path.join(element.path,element.name));
+                console.log("  |".repeat(count),element.name,dataEntity(fileStat.mtimeMs));
+            }
+        });
+    }
